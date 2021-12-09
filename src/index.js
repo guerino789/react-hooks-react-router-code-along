6dbfb5adf18b4f5f5af8bfd693da06300075e53f
guerino <guerantonini94@gmail.com>
2021-12-09 17:20:22 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import App from "./componets/App";
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/">
-      <Home />
-    </Route>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
